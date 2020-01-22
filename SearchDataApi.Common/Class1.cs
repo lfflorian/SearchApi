20b3feacece92d0145462a8b1d625f42e3cc965f
Luis Florian <lfflorian4@gmail.com>
2020-01-22 11:56:51 -0600

    Inicialización de proyectos de estructura

---
+++ SearchDataApi.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SearchDataApi.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ SearchDataApi.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SearchDataApi.Services
+{
+    public class Class1
+    {
+    }
+}
